76d5f571b1e87ac00d4d0bfad325754932e36a50
Roman Imankulov <roman.imankulov@gmail.com>
2023-10-05 17:44:07 +0100

    Add message component

---
+++ sample/myapp/components/message/message.py
@@ -0,0 +1,23 @@
+from django_components import component
+from pydantic import BaseModel
+
+from livecomponents import LiveComponent
+from livecomponents.manager.manager import CallContext
+
+
+class MessageState(BaseModel):
+    message: str | None = None
+
+
+@component.register("message")
+class MessageComponent(LiveComponent[MessageState]):
+    template_name = "message/message.html"
+    state_cls = MessageState
+
+    @classmethod
+    def init_state(cls) -> MessageState:
+        return MessageState()
+
+    @classmethod
+    def set_message(cls, call_context: CallContext, message: str):
+        call_context.state.message = message
