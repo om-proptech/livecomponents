76d64a59e3b07fa89b8be3f077b8dd702418a102
Roman Imankulov <roman.imankulov@gmail.com>
2023-10-06 11:01:04 +0100

    Fall back to ids supporting "#" and "/"

--- livecomponents/const.py
+++ livecomponents/const.py
@@ -1,10 +1,10 @@
-# We would love to use HIER_SEP = "/" and TYPE_SEP = ":"
-# so that the full ID look like this:
+# The full ID look like this:
 # "parent-type:parent-id/child-type:child-id"
 
-# However, HTMX doesn't escape query selectors, so we're forced to use
-# something else. See:
+#  HTMX doesn't escape query selectors, so elsewhere in the code we
+# have to explicitly oass the escape the "hx-swap-oob" attributes
+# as "hx-swap-oob="morph:#parent-type\:parent-id\/child-type\:child-id"
 # https://github.com/bigskysoftware/htmx/issues/1537
 
-HIER_SEP = "-CHILD-"
-TYPE_SEP = "-ID-"
+HIER_SEP = "/"
+TYPE_SEP = ":"
