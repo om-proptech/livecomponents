0d6981776ac02caef17e50c4cce2de23f12b0a4e
Roman Imankulov <roman.imankulov@gmail.com>
2023-10-17 13:20:53 +0100

    Generate a safer session ID

--- livecomponents/sessions.py
+++ livecomponents/sessions.py
@@ -1,9 +1,9 @@
-import uuid
+import secrets
 
 from django.http import HttpRequest
 
 
 def get_session_id(request: HttpRequest):
-    """Return the session ID for the live components session."""
+    """Return the session ID for the livecomponent session."""
     # Generate a random session ID for every page reload.
-    return uuid.uuid4().hex
+    return secrets.token_urlsafe(16)
