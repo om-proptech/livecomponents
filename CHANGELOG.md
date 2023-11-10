07fe5db19717aed446579f096b023b9f97e55850
Roman Imankulov <roman.imankulov@gmail.com>
2023-11-10 16:39:22 +0000

    Make UpdateStateContext importable directly from livecomponents

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## UNRELEASED
+
+- Made UpdateStateContext importable directly from livecomponents
+
 ## 1.4.0 (2023-11-10)
 
 - Made it possible to re-render the component state on re-render.
--- livecomponents/__init__.py
+++ livecomponents/__init__.py
@@ -1,4 +1,14 @@
 from livecomponents.component import LiveComponent, command
-from livecomponents.manager.manager import CallContext, InitStateContext
+from livecomponents.manager.manager import (
+    CallContext,
+    InitStateContext,
+    UpdateStateContext,
+)
 
-__all__ = ["LiveComponent", "command", "CallContext", "InitStateContext"]
+__all__ = [
+    "LiveComponent",
+    "command",
+    "CallContext",
+    "InitStateContext",
+    "UpdateStateContext",
+]
